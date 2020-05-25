500843d8e98d264c95536be43eae83c877f45056
Leon Merfort <58845874+merfort@users.noreply.github.com>
2020-05-25 15:14:27 +0200

    Updated config files and compareScenarios.R (work in progress)

--- scripts/output/comparison/compareScenarios.R
+++ scripts/output/comparison/compareScenarios.R
@@ -15,20 +15,100 @@ if (exists("outputdirs")) {
 } else {
   # This is the case if this script was called directly via Rscript
   listofruns <- list( 
-      list(period = "both",  set = "cpl-Base",       dirs = c("C_SDP-Base-rem-5",       "C_SSP1-Base-rem-5",       "C_SSP2-Base-rem-5",       "C_SSP5-Base-rem-5")),
-      list(period = "both",  set = "cpl-PkBudg900",  dirs = c("C_SDP-PkBudg900-rem-5",  "C_SSP1-PkBudg900-rem-5",  "C_SSP2-PkBudg900-rem-5",  "C_SSP5-PkBudg900-rem-5")),
-      list(period = "both",  set = "cpl-PkBudg1100", dirs = c("C_SDP-PkBudg1100-rem-5", "C_SSP1-PkBudg1100-rem-5", "C_SSP2-PkBudg1100-rem-5", "C_SSP5-PkBudg1100-rem-5")),
-      list(period = "both",  set = "cpl-PkBudg1300", dirs = c("C_SDP-PkBudg1300-rem-5", "C_SSP1-PkBudg1300-rem-5", "C_SSP2-PkBudg1300-rem-5", "C_SSP5-PkBudg1300-rem-5")),
-      list(period = "both",  set = "cpl-NPi",        dirs = c("C_SDP-NPi-rem-5",        "C_SSP1-NPi-rem-5",        "C_SSP2-NPi-rem-5",        "C_SSP5-NPi-rem-5")),
-      
-      list(period = "both",  set = "cpl-SDP",  dirs = c("C_SDP-Base-rem-5",  "C_SDP-NPi-rem-5",  "C_SDP-PkBudg1300-rem-5",  "C_SDP-PkBudg1100-rem-5",  "C_SDP-PkBudg1000-rem-5")),
-      list(period = "both",  set = "cpl-SSP1", dirs = c("C_SSP1-Base-rem-5", "C_SSP1-NPi-rem-5", "C_SSP1-PkBudg1300-rem-5", "C_SSP1-PkBudg1100-rem-5", "C_SSP1-PkBudg900-rem-5")),
-      list(period = "both",  set = "cpl-SSP2", dirs = c("C_SSP2-Base-rem-5", "C_SSP2-NPi-rem-5", "C_SSP2-PkBudg1300-rem-5", "C_SSP2-PkBudg1100-rem-5", "C_SSP2-PkBudg900-rem-5", "C_SSP2-NDC-rem-5")), #
-      list(period = "both",  set = "cpl-SSP5", dirs = c("C_SSP5-Base-rem-5", "C_SSP5-NPi-rem-5", "C_SSP5-PkBudg1300-rem-5", "C_SSP5-PkBudg1100-rem-5", "C_SSP5-PkBudg900-rem-5")),
-      
-      #list(period = "both",  set = "cpl-PkBudg900-plant-vgl",  dirs = c("C_SDP-PkBudg900-plant-rem-5", "C_SDP-PkBudg900-rem-5", "C_SSP2-PkBudg900-plant-rem-5", "C_SSP2-PkBudg900-rem-5")),
-      #list(period = "both",  set = "cpl-PkBudg900-plant",      dirs = c("C_SDP-PkBudg900-plant-rem-5",  "C_SSP1-PkBudg900-plant-rem-5",  "C_SSP2-PkBudg900-plant-rem-5",  "C_SSP5-PkBudg900-plant-rem-5")),
-      NULL)
+      # list(period = "both",  set = "cpl-Base",       dirs = c("C_SDP-Base-rem-5",       "C_SSP1-Base-rem-5",       "C_SSP2-Base-rem-5",       "C_SSP5-Base-rem-5")),
+      # list(period = "both",  set = "cpl-PkBudg900",  dirs = c("C_SDP-PkBudg900-rem-5",  "C_SSP1-PkBudg900-rem-5",  "C_SSP2-PkBudg900-rem-5",  "C_SSP5-PkBudg900-rem-5")),
+      # list(period = "both",  set = "cpl-PkBudg1100", dirs = c("C_SDP-PkBudg1100-rem-5", "C_SSP1-PkBudg1100-rem-5", "C_SSP2-PkBudg1100-rem-5", "C_SSP5-PkBudg1100-rem-5")),
+      # list(period = "both",  set = "cpl-PkBudg1300", dirs = c("C_SDP-PkBudg1300-rem-5", "C_SSP1-PkBudg1300-rem-5", "C_SSP2-PkBudg1300-rem-5", "C_SSP5-PkBudg1300-rem-5")),
+      # list(period = "both",  set = "cpl-NPi",        dirs = c("C_SDP-NPi-rem-5",        "C_SSP1-NPi-rem-5",        "C_SSP2-NPi-rem-5",        "C_SSP5-NPi-rem-5")),
+      # 
+      # list(period = "both",  set = "cpl-SDP",  dirs = c("C_SDP-Base-rem-5",  "C_SDP-NPi-rem-5",  "C_SDP-PkBudg1300-rem-5",  "C_SDP-PkBudg1100-rem-5",  "C_SDP-PkBudg1000-rem-5")),
+      # list(period = "both",  set = "cpl-SSP1", dirs = c("C_SSP1-Base-rem-5", "C_SSP1-NPi-rem-5", "C_SSP1-PkBudg1300-rem-5", "C_SSP1-PkBudg1100-rem-5", "C_SSP1-PkBudg900-rem-5")),
+      # list(period = "both",  set = "cpl-SSP2", dirs = c("C_SSP2-Base-rem-5", "C_SSP2-NPi-rem-5", "C_SSP2-PkBudg1300-rem-5", "C_SSP2-PkBudg1100-rem-5", "C_SSP2-PkBudg900-rem-5", "C_SSP2-NDC-rem-5")), #
+      # list(period = "both",  set = "cpl-SSP5", dirs = c("C_SSP5-Base-rem-5", "C_SSP5-NPi-rem-5", "C_SSP5-PkBudg1300-rem-5", "C_SSP5-PkBudg1100-rem-5", "C_SSP5-PkBudg900-rem-5")),
+    
+    # list(period = "both",  set = "cpl-PkBudg900-plant-vgl",  dirs = c("C_SDP-PkBudg900-plant-rem-5", "C_SDP-PkBudg900-rem-5", "C_SSP2-PkBudg900-plant-rem-5", "C_SSP2-PkBudg900-rem-5")),
+    # list(period = "both",  set = "cpl-PkBudg900-plant",      dirs = c("C_SDP-PkBudg900-plant-rem-5",  "C_SSP1-PkBudg900-plant-rem-5",  "C_SSP2-PkBudg900-plant-rem-5",  "C_SSP5-PkBudg900-plant-rem-5")),
+    
+    # list(period = "long",  set = "Base-NDC_branch100-develop",  dirs = c("./output/SSP2-Base_coemi100_2020-04-08_14.18.44",
+    #                                                                      "../remind_develop/output/SSP2-Base_2020-04-08_12.29.26",
+    #                                                                      "./output/SSP2-NDC_coemi100_2020-04-08_14.54.56",
+    #                                                                      "../remind_develop/output/SSP2-NDC_2020-04-08_13.08.54")),
+    
+    # list(period = "long",  set = "Base-coemi-CO2_21p4_N2O-7p5", dirs = c(
+    #   "C_SSP2-Base-rem-5",
+    #   "C_SSP2-Base_coemi-NPI-Budg1300-rem-5"
+    # )),
+    # list(period = "long",  set = "NDC-coemi-CO2_21p4_N2O-7p5", dirs = c(
+    #   "C_SSP2-NDC-rem-5",
+    #   "C_SSP2-NDC_coemi-NPI-Budg1300-rem-5",
+    #   "C_SSP2-NDC_Mag-noTaxCO2N2O_tau1p5-rem-5",
+    #   "C_SSP2-NDC_Mag-noTaxCO2N2O_tau0-rem-5"
+    # )),
+    # list(period = "long",  set = "PkBudg1300-coemi-CO2_21p4_N2O-7p5", dirs = c(
+    #   "C_SSP2-PkBudg1300-rem-5",
+    #   "C_SSP2-PkBudg1300_coemi-NPI-Budg1300-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_tau1p5-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_tau0-rem-5"
+    # )),
+    # list(period = "long",  set = "PkBudg1300-efCO2range-incl-all", dirs = c(
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-00-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-05-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-10-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-20-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-30-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-40-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-50-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-60-rem-5",
+    #   "C_SSP2-PkBudg1300-rem-5",
+    #   "C_SSP2-Base-rem-5"
+    # )),
+    # list(period = "long",  set = "PkBudg1300-effect-of-tau-runs", dirs = c(
+    #   "C_SSP2-Base-rem-5",
+    #   "C_SSP2-Base_tau0-rem-5",
+    #   "C_SSP2-NDC-rem-5",
+    #   "C_SSP2-NDC_tau0-rem-5",
+    #   "C_SSP2-PkBudg1300-rem-5",
+    #   "C_SSP2-PkBudg1300_tau0-rem-5"
+    # )),
+    # list(period = "long",  set = "PkBudg1300-effect-of-tau-runs-plus-coemi", dirs = c(
+    #   "C_SSP2-Base-rem-5",
+    #   "C_SSP2-Base_tau0-rem-5",
+    #   "C_SSP2-NDC-rem-5",
+    #   "C_SSP2-NDC_tau0-rem-5",
+    #   "C_SSP2-PkBudg1300-rem-5",
+    #   "C_SSP2-PkBudg1300_tau0-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-00-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_efCO2-60-rem-5"
+    # )),
+    # list(period = "long",  set = "PkBudg1300-allTaxOff-vs-noTaxCO2andN2O-tau0", dirs = c(
+    #   "C_SSP2-Base_Mag-allTaxOff_tau0-rem-5",
+    #   "C_SSP2-Base_Mag-noTaxCO2N2O_tau0-rem-5",
+    #   "C_SSP2-NDC_Mag-allTaxOff_tau0-rem-5",
+    #   "C_SSP2-NDC_Mag-noTaxCO2N2O_tau0-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-allTaxOff_tau0-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_tau0-rem-5",
+    #   "C_SSP2-Base-rem-5",
+    #   "C_SSP2-PkBudg1300-rem-5"
+    # )),
+    # list(period = "long",  set = "PkBudg1300-allTaxOff-vs-noTaxCO2andN2O-tau1p5", dirs = c(
+    #   "C_SSP2-Base_Mag-allTaxOff_tau1p5-rem-5",
+    #   "C_SSP2-Base_Mag-noTaxCO2N2O_tau1p5-rem-5",
+    #   "C_SSP2-NDC_Mag-allTaxOff_tau1p5-rem-5",
+    #   "C_SSP2-NDC_Mag-noTaxCO2N2O_tau1p5-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-allTaxOff_tau1p5-rem-5",
+    #   "C_SSP2-PkBudg1300_Mag-noTaxCO2N2O_tau1p5-rem-5",
+    #   "C_SSP2-Base-rem-5",
+    #   "C_SSP2-PkBudg1300-rem-5"
+    # )),
+    list(period = "long",  set = "PkBudg900-1100-1300", dirs = c(
+      "C_SSP2-PkBudg900-rem-5",
+      "C_SSP2-PkBudg900_tau0-rem-5",
+      "C_SSP2-PkBudg1100-rem-5",
+      "C_SSP2-PkBudg1100_tau0-rem-5",
+      "C_SSP2-PkBudg1300-rem-5",
+      "C_SSP2-PkBudg1300_tau0-rem-5"
+    )),
+    NULL)
 }
 
 # remove the NULL element
