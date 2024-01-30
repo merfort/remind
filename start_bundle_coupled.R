829c9dd67e47dc133f14fcc1a328cebc660ece6d
Leon Merfort <58845874+merfort@users.noreply.github.com>
2024-01-30 11:37:53 +0100

    Set number of coupling iterations back to 5

--- start_bundle_coupled.R
+++ start_bundle_coupled.R
@@ -79,7 +79,7 @@ path_magpie_oldruns <- paste0(path_magpie, "output/")
 prefix_oldruns <-  "C_"
 
 # number of coupling iterations, can also be specified in path_settings_coupled
-max_iterations <- 8
+max_iterations <- 5
 
 # Number of coupling iterations (before final iteration) in which MAgPIE uses higher n600 resolution.
 # Until "max_iterations - n600_iterations" iteration MAgPIE runs with n200 resolution.
