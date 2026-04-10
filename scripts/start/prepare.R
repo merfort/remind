6eaaff7dd7c8c62b52135ea0187792f8d4fc2bcf
Leon Merfort <58845874+merfort@users.noreply.github.com>
2026-04-10 10:12:17 +0200

    Increase model lock timeout value

--- scripts/start/prepare.R
+++ scripts/start/prepare.R
@@ -83,7 +83,7 @@ prepare <- function() {
 
   ################## M O D E L   L O C K ###################################
   # Lock the directory for other instances of the start scripts
-  lock_id <- model_lock(timeout1 = 4)
+  lock_id <- model_lock(timeout1 = 10)
   on.exit() # set the commands when exiting in the correct order
   on.exit(model_unlock(lock_id),add=TRUE)
   on.exit(setwd(cfg$results_folder),add=TRUE)
